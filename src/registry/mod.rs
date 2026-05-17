38dc25e0cb7ba3bd1d0037a7a033bc26a753f433
trentmkelly <trent.michael.kelly@gmail.com>
2026-05-17 01:30:22 -0400

    Preserve vanilla registry ordering

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -503,6 +503,15 @@ pub struct DataPackRegistryEntry {
 #[derive(Debug, Clone)]
 pub struct DynamicRegistryAccess {
     registries: BTreeMap<Identifier, Registry<String>>,
+    order: Vec<Identifier>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct BuiltInRegistryDescriptor {
+    pub java_field: &'static str,
+    pub registry: &'static str,
+    pub default_key: Option<&'static str>,
+    pub intrusive_holders: bool,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -620,6 +629,7 @@ fn vanilla_reload_stages() -> Vec<ServerReloadStage> {
 impl DynamicRegistryAccess {
     pub fn from_builtins(builtins: &BuiltInRegistries) -> Self {
         let mut registries = BTreeMap::new();
+        let mut order = Vec::new();
         for registry in [
             builtins.blocks.clone(),
             builtins.items.clone(),
@@ -627,9 +637,10 @@ impl DynamicRegistryAccess {
             builtins.dimension_types.clone(),
             builtins.biomes.clone(),
         ] {
+            order.push(registry.registry_id().clone());
             registries.insert(registry.registry_id().clone(), registry);
         }
-        Self { registries }
+        Self { registries, order }
     }
 
     pub fn apply_data_pack_entries(
@@ -638,6 +649,9 @@ impl DynamicRegistryAccess {
     ) -> Result<(), String> {
         for entry in entries {
             let registry_id = entry.registry.clone();
+            if !self.registries.contains_key(&registry_id) {
+                self.order.push(registry_id.clone());
+            }
             let registry = self
                 .registries
                 .entry(registry_id.clone())
@@ -662,7 +676,448 @@ impl DynamicRegistryAccess {
     }
 
     pub fn registry_ids(&self) -> Vec<Identifier> {
-        self.registries.keys().cloned().collect()
+        self.order.clone()
+    }
+}
+
+pub fn builtin_registry_manifest_26_1_2() -> Vec<BuiltInRegistryDescriptor> {
+    BUILTIN_REGISTRY_MANIFEST_26_1_2.to_vec()
+}
+
+static BUILTIN_REGISTRY_MANIFEST_26_1_2: &[BuiltInRegistryDescriptor] = &[
+    descriptor("GAME_EVENT", "minecraft:game_event", Some("step"), false),
+    descriptor("SOUND_EVENT", "minecraft:sound_event", None, false),
+    descriptor("FLUID", "minecraft:fluid", Some("empty"), true),
+    descriptor("MOB_EFFECT", "minecraft:mob_effect", None, false),
+    descriptor("BLOCK", "minecraft:block", Some("air"), true),
+    descriptor(
+        "DEBUG_SUBSCRIPTION",
+        "minecraft:debug_subscription",
+        None,
+        false,
+    ),
+    descriptor("ENTITY_TYPE", "minecraft:entity_type", Some("pig"), true),
+    descriptor("ITEM", "minecraft:item", Some("air"), true),
+    descriptor("POTION", "minecraft:potion", None, false),
+    descriptor("PARTICLE_TYPE", "minecraft:particle_type", None, false),
+    descriptor(
+        "BLOCK_ENTITY_TYPE",
+        "minecraft:block_entity_type",
+        None,
+        true,
+    ),
+    descriptor("CUSTOM_STAT", "minecraft:custom_stat", None, false),
+    descriptor(
+        "CHUNK_STATUS",
+        "minecraft:chunk_status",
+        Some("empty"),
+        false,
+    ),
+    descriptor("RULE_TEST", "minecraft:rule_test", None, false),
+    descriptor(
+        "RULE_BLOCK_ENTITY_MODIFIER",
+        "minecraft:rule_block_entity_modifier",
+        None,
+        false,
+    ),
+    descriptor("POS_RULE_TEST", "minecraft:pos_rule_test", None, false),
+    descriptor("MENU", "minecraft:menu", None, false),
+    descriptor("RECIPE_TYPE", "minecraft:recipe_type", None, false),
+    descriptor(
+        "RECIPE_SERIALIZER",
+        "minecraft:recipe_serializer",
+        None,
+        false,
+    ),
+    descriptor("ATTRIBUTE", "minecraft:attribute", None, false),
+    descriptor(
+        "POSITION_SOURCE_TYPE",
+        "minecraft:position_source_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "COMMAND_ARGUMENT_TYPE",
+        "minecraft:command_argument_type",
+        None,
+        false,
+    ),
+    descriptor("STAT_TYPE", "minecraft:stat_type", None, false),
+    descriptor(
+        "VILLAGER_TYPE",
+        "minecraft:villager_type",
+        Some("plains"),
+        false,
+    ),
+    descriptor(
+        "VILLAGER_PROFESSION",
+        "minecraft:villager_profession",
+        Some("none"),
+        false,
+    ),
+    descriptor(
+        "POINT_OF_INTEREST_TYPE",
+        "minecraft:point_of_interest_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "MEMORY_MODULE_TYPE",
+        "minecraft:memory_module_type",
+        Some("dummy"),
+        false,
+    ),
+    descriptor("SENSOR_TYPE", "minecraft:sensor_type", Some("dummy"), false),
+    descriptor("ACTIVITY", "minecraft:activity", None, false),
+    descriptor(
+        "LOOT_POOL_ENTRY_TYPE",
+        "minecraft:loot_pool_entry_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "LOOT_FUNCTION_TYPE",
+        "minecraft:loot_function_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "LOOT_CONDITION_TYPE",
+        "minecraft:loot_condition_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "LOOT_NUMBER_PROVIDER_TYPE",
+        "minecraft:loot_number_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "LOOT_NBT_PROVIDER_TYPE",
+        "minecraft:loot_nbt_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "LOOT_SCORE_PROVIDER_TYPE",
+        "minecraft:loot_score_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "FLOAT_PROVIDER_TYPE",
+        "minecraft:float_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "INT_PROVIDER_TYPE",
+        "minecraft:int_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "HEIGHT_PROVIDER_TYPE",
+        "minecraft:height_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "BLOCK_PREDICATE_TYPE",
+        "minecraft:block_predicate_type",
+        None,
+        false,
+    ),
+    descriptor("CARVER", "minecraft:worldgen/carver", None, false),
+    descriptor("FEATURE", "minecraft:worldgen/feature", None, false),
+    descriptor(
+        "STRUCTURE_PLACEMENT",
+        "minecraft:worldgen/structure_placement",
+        None,
+        false,
+    ),
+    descriptor(
+        "STRUCTURE_PIECE",
+        "minecraft:worldgen/structure_piece",
+        None,
+        false,
+    ),
+    descriptor(
+        "STRUCTURE_TYPE",
+        "minecraft:worldgen/structure_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "PLACEMENT_MODIFIER_TYPE",
+        "minecraft:worldgen/placement_modifier_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "BLOCKSTATE_PROVIDER_TYPE",
+        "minecraft:worldgen/block_state_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "FOLIAGE_PLACER_TYPE",
+        "minecraft:worldgen/foliage_placer_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "TRUNK_PLACER_TYPE",
+        "minecraft:worldgen/trunk_placer_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ROOT_PLACER_TYPE",
+        "minecraft:worldgen/root_placer_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "TREE_DECORATOR_TYPE",
+        "minecraft:worldgen/tree_decorator_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "FEATURE_SIZE_TYPE",
+        "minecraft:worldgen/feature_size_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "BIOME_SOURCE",
+        "minecraft:worldgen/biome_source",
+        None,
+        false,
+    ),
+    descriptor(
+        "CHUNK_GENERATOR",
+        "minecraft:worldgen/chunk_generator",
+        None,
+        false,
+    ),
+    descriptor(
+        "MATERIAL_CONDITION",
+        "minecraft:worldgen/material_condition",
+        None,
+        false,
+    ),
+    descriptor(
+        "MATERIAL_RULE",
+        "minecraft:worldgen/material_rule",
+        None,
+        false,
+    ),
+    descriptor(
+        "DENSITY_FUNCTION_TYPE",
+        "minecraft:worldgen/density_function_type",
+        None,
+        false,
+    ),
+    descriptor("BLOCK_TYPE", "minecraft:block_type", None, false),
+    descriptor(
+        "STRUCTURE_PROCESSOR",
+        "minecraft:worldgen/structure_processor",
+        None,
+        false,
+    ),
+    descriptor(
+        "STRUCTURE_POOL_ELEMENT",
+        "minecraft:worldgen/structure_pool_element",
+        None,
+        false,
+    ),
+    descriptor(
+        "POOL_ALIAS_BINDING_TYPE",
+        "minecraft:worldgen/pool_alias_binding",
+        None,
+        false,
+    ),
+    descriptor(
+        "DECORATED_POT_PATTERN",
+        "minecraft:decorated_pot_pattern",
+        None,
+        false,
+    ),
+    descriptor(
+        "CREATIVE_MODE_TAB",
+        "minecraft:creative_mode_tab",
+        None,
+        false,
+    ),
+    descriptor("TRIGGER_TYPES", "minecraft:trigger_type", None, false),
+    descriptor(
+        "NUMBER_FORMAT_TYPE",
+        "minecraft:number_format_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "DATA_COMPONENT_TYPE",
+        "minecraft:data_component_type",
+        None,
+        false,
+    ),
+    descriptor("GAME_RULE", "minecraft:game_rule", None, false),
+    descriptor(
+        "ENTITY_SUB_PREDICATE_TYPE",
+        "minecraft:entity_sub_predicate_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "DATA_COMPONENT_PREDICATE_TYPE",
+        "minecraft:data_component_predicate_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "MAP_DECORATION_TYPE",
+        "minecraft:map_decoration_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ENCHANTMENT_EFFECT_COMPONENT_TYPE",
+        "minecraft:enchantment_effect_component_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ENCHANTMENT_LEVEL_BASED_VALUE_TYPE",
+        "minecraft:enchantment_level_based_value_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ENCHANTMENT_ENTITY_EFFECT_TYPE",
+        "minecraft:enchantment_entity_effect_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ENCHANTMENT_LOCATION_BASED_EFFECT_TYPE",
+        "minecraft:enchantment_location_based_effect_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ENCHANTMENT_VALUE_EFFECT_TYPE",
+        "minecraft:enchantment_value_effect_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ENCHANTMENT_PROVIDER_TYPE",
+        "minecraft:enchantment_provider_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "CONSUME_EFFECT_TYPE",
+        "minecraft:consume_effect_type",
+        None,
+        false,
+    ),
+    descriptor("RECIPE_DISPLAY", "minecraft:recipe_display", None, false),
+    descriptor("SLOT_DISPLAY", "minecraft:slot_display", None, false),
+    descriptor(
+        "RECIPE_BOOK_CATEGORY",
+        "minecraft:recipe_book_category",
+        None,
+        false,
+    ),
+    descriptor("TICKET_TYPE", "minecraft:ticket_type", None, false),
+    descriptor(
+        "INCOMING_RPC_METHOD",
+        "minecraft:incoming_rpc_methods",
+        None,
+        false,
+    ),
+    descriptor(
+        "OUTGOING_RPC_METHOD",
+        "minecraft:outgoing_rpc_methods",
+        None,
+        false,
+    ),
+    descriptor(
+        "TEST_ENVIRONMENT_DEFINITION_TYPE",
+        "minecraft:test_environment_definition_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "TEST_INSTANCE_TYPE",
+        "minecraft:test_instance_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "SPAWN_CONDITION_TYPE",
+        "minecraft:spawn_condition_type",
+        None,
+        false,
+    ),
+    descriptor("DIALOG_TYPE", "minecraft:dialog_type", None, false),
+    descriptor(
+        "DIALOG_ACTION_TYPE",
+        "minecraft:dialog_action_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "INPUT_CONTROL_TYPE",
+        "minecraft:input_control_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "DIALOG_BODY_TYPE",
+        "minecraft:dialog_body_type",
+        None,
+        false,
+    ),
+    descriptor("PERMISSION_TYPE", "minecraft:permission_type", None, false),
+    descriptor(
+        "PERMISSION_CHECK_TYPE",
+        "minecraft:permission_check_type",
+        None,
+        false,
+    ),
+    descriptor(
+        "ENVIRONMENT_ATTRIBUTE",
+        "minecraft:environment_attribute",
+        None,
+        false,
+    ),
+    descriptor("ATTRIBUTE_TYPE", "minecraft:attribute_type", None, false),
+    descriptor(
+        "SLOT_SOURCE_TYPE",
+        "minecraft:slot_source_type",
+        None,
+        false,
+    ),
+    descriptor("TEST_FUNCTION", "minecraft:test_function", None, false),
+];
+
+const fn descriptor(
+    java_field: &'static str,
+    registry: &'static str,
+    default_key: Option<&'static str>,
+    intrusive_holders: bool,
+) -> BuiltInRegistryDescriptor {
+    BuiltInRegistryDescriptor {
+        java_field,
+        registry,
+        default_key,
+        intrusive_holders,
     }
 }
 
@@ -916,8 +1371,8 @@ pub mod feature_flags {
 #[cfg(test)]
 mod tests {
     use super::{
-        feature_flags, registries, FeatureFlagRegistry, Holder, HolderSet, Identifier, Lifecycle,
-        Registry, TagKey,
+        builtin_registry_manifest_26_1_2, feature_flags, registries, FeatureFlagRegistry, Holder,
+        HolderSet, Identifier, Lifecycle, Registry, TagKey,
     };
 
     #[test]
@@ -1142,6 +1597,34 @@ mod tests {
     }
 
     #[test]
+    fn builtin_registry_manifest_matches_26_1_2_builtin_registration_order() {
+        let manifest = builtin_registry_manifest_26_1_2();
+        assert_eq!(manifest.len(), 95);
+        assert_eq!(manifest[0].java_field, "GAME_EVENT");
+        assert_eq!(manifest[0].registry, "minecraft:game_event");
+        assert_eq!(manifest[0].default_key, Some("step"));
+        assert_eq!(manifest[4].java_field, "BLOCK");
+        assert_eq!(manifest[4].registry, "minecraft:block");
+        assert_eq!(manifest[4].default_key, Some("air"));
+        assert!(manifest[4].intrusive_holders);
+        assert_eq!(manifest[6].java_field, "ENTITY_TYPE");
+        assert_eq!(manifest[7].java_field, "ITEM");
+        assert_eq!(manifest[56].java_field, "BLOCK_TYPE");
+        assert_eq!(manifest[81].registry, "minecraft:outgoing_rpc_methods");
+        assert_eq!(manifest.last().unwrap().java_field, "TEST_FUNCTION");
+
+        let mut seen = std::collections::BTreeSet::new();
+        for descriptor in &manifest {
+            assert!(
+                seen.insert(descriptor.registry),
+                "duplicate registry {}",
+                descriptor.registry
+            );
+            Identifier::parse(descriptor.registry).unwrap();
+        }
+    }
+
+    #[test]
     fn dynamic_registry_access_applies_datapack_entries_over_builtins() {
         let builtins = super::BuiltInRegistries::bootstrap_26_1_2();
         let mut dynamic = super::DynamicRegistryAccess::from_builtins(&builtins);
@@ -1186,6 +1669,47 @@ mod tests {
             .registry(&Identifier::parse(registries::BIOME).unwrap())
             .unwrap()
             .is_frozen());
+    }
+
+    #[test]
+    fn dynamic_registry_access_preserves_builtin_then_datapack_registry_order() {
+        let builtins = super::BuiltInRegistries::bootstrap_26_1_2();
+        let mut dynamic = super::DynamicRegistryAccess::from_builtins(&builtins);
+        dynamic
+            .apply_data_pack_entries(vec![
+                super::DataPackRegistryEntry {
+                    registry: Identifier::parse("minecraft:chat_type").unwrap(),
+                    location: Identifier::parse("minecraft:chat").unwrap(),
+                    value: "chat codec".to_string(),
+                    lifecycle: Lifecycle::Stable,
+                },
+                super::DataPackRegistryEntry {
+                    registry: Identifier::parse(registries::BIOME).unwrap(),
+                    location: Identifier::parse("minecraft:forest").unwrap(),
+                    value: "pack forest".to_string(),
+                    lifecycle: Lifecycle::Stable,
+                },
+                super::DataPackRegistryEntry {
+                    registry: Identifier::parse("minecraft:damage_type").unwrap(),
+                    location: Identifier::parse("minecraft:generic").unwrap(),
+                    value: "damage codec".to_string(),
+                    lifecycle: Lifecycle::Stable,
+                },
+            ])
+            .unwrap();
+
+        assert_eq!(
+            dynamic.registry_ids(),
+            vec![
+                Identifier::parse(registries::BLOCK).unwrap(),
+                Identifier::parse(registries::ITEM).unwrap(),
+                Identifier::parse(registries::ENTITY_TYPE).unwrap(),
+                Identifier::parse(registries::DIMENSION_TYPE).unwrap(),
+                Identifier::parse(registries::BIOME).unwrap(),
+                Identifier::parse("minecraft:chat_type").unwrap(),
+                Identifier::parse("minecraft:damage_type").unwrap(),
+            ]
+        );
     }
 
     #[test]
