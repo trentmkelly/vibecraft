0a265866b1505c27c954a279da1f96637700918b
trentmkelly <trent.michael.kelly@gmail.com>
2026-05-18 01:38:49 -0400

    Add overworld multi-noise biome presets

--- src/biome.rs
+++ src/biome.rs
@@ -39,11 +39,31 @@ pub struct ClimateBiomeEntry {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct MultiNoiseBiomeSourceParameterListPreset {
+    pub id: &'static str,
+    pub parameters: &'static [ClimateBiomeEntry],
+    pub used_biomes: &'static [&'static str],
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ClimateSamplerSample {
     pub quart_x: i32,
     pub quart_y: i32,
     pub quart_z: i32,
+    pub block_x: i32,
+    pub block_y: i32,
+    pub block_z: i32,
     pub target: ClimateTarget,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct ClimateSamplerInput {
+    pub temperature: f32,
+    pub humidity: f32,
+    pub continentalness: f32,
+    pub erosion: f32,
+    pub depth: f32,
+    pub weirdness: f32,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -295,12 +315,236 @@ pub const NETHER_BIOME_PARAMETERS: &[ClimateBiomeEntry] = &[
     },
 ];
 
+pub const OVERWORLD_BIOME_PARAMETERS: &[ClimateBiomeEntry] = &[
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-1.0, 1.0),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-1.2, -1.05),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:mushroom_fields",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-1.0, -0.45),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-1.05, -0.455),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:deep_frozen_ocean",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-0.45, -0.15),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-1.05, -0.455),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:deep_cold_ocean",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-0.15, 0.2),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-1.05, -0.455),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:deep_ocean",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(0.2, 0.55),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-0.455, -0.19),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:lukewarm_ocean",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(0.55, 1.0),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-0.455, -0.19),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:warm_ocean",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-1.0, -0.45),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-0.11, 0.55),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:snowy_plains",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-0.45, -0.15),
+            humidity: span(-1.0, -0.35),
+            continentalness: span(-0.11, 0.55),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:plains",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-0.15, 0.2),
+            humidity: span(-0.1, 0.1),
+            continentalness: span(-0.11, 0.55),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:forest",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(0.2, 0.55),
+            humidity: span(-1.0, -0.35),
+            continentalness: span(0.3, 1.0),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:savanna",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(0.55, 1.0),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-0.11, 1.0),
+            erosion: span(-1.0, 1.0),
+            depth: point(0.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:desert",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-1.0, 1.0),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-1.0, 1.0),
+            erosion: span(-1.0, 1.0),
+            depth: span(0.2, 0.9),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:dripstone_caves",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-1.0, 1.0),
+            humidity: span(0.3, 1.0),
+            continentalness: span(-1.0, 1.0),
+            erosion: span(-1.0, 1.0),
+            depth: span(0.2, 0.9),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:lush_caves",
+    },
+    ClimateBiomeEntry {
+        parameters: ClimateParameterPoint {
+            temperature: span(-1.0, 1.0),
+            humidity: span(-1.0, 1.0),
+            continentalness: span(-1.0, 1.0),
+            erosion: span(-1.0, -0.225),
+            depth: span(0.9, 1.0),
+            weirdness: span(-1.0, 1.0),
+            offset: 0,
+        },
+        biome: "minecraft:deep_dark",
+    },
+];
+
+pub const NETHER_MULTI_NOISE_USED_BIOMES: &[&str] = &[
+    "minecraft:nether_wastes",
+    "minecraft:soul_sand_valley",
+    "minecraft:crimson_forest",
+    "minecraft:warped_forest",
+    "minecraft:basalt_deltas",
+];
+
+pub const OVERWORLD_MULTI_NOISE_USED_BIOMES: &[&str] = &[
+    "minecraft:mushroom_fields",
+    "minecraft:deep_frozen_ocean",
+    "minecraft:deep_cold_ocean",
+    "minecraft:deep_ocean",
+    "minecraft:lukewarm_ocean",
+    "minecraft:warm_ocean",
+    "minecraft:snowy_plains",
+    "minecraft:plains",
+    "minecraft:forest",
+    "minecraft:savanna",
+    "minecraft:desert",
+    "minecraft:dripstone_caves",
+    "minecraft:lush_caves",
+    "minecraft:deep_dark",
+];
+
 pub const fn quantize_coord(coord: f32) -> i64 {
     (coord * QUANTIZATION_FACTOR) as i64
 }
 
+pub const fn quart_to_block(coord: i32) -> i32 {
+    coord * 4
+}
+
 pub fn unquantize_coord(coord: i64) -> f32 {
     coord as f32 / QUANTIZATION_FACTOR
+}
+
+pub const fn climate_sampler_sample(
+    quart_x: i32,
+    quart_y: i32,
+    quart_z: i32,
+    input: ClimateSamplerInput,
+) -> ClimateSamplerSample {
+    ClimateSamplerSample {
+        quart_x,
+        quart_y,
+        quart_z,
+        block_x: quart_to_block(quart_x),
+        block_y: quart_to_block(quart_y),
+        block_z: quart_to_block(quart_z),
+        target: climate_target(
+            input.temperature,
+            input.humidity,
+            input.continentalness,
+            input.erosion,
+            input.depth,
+            input.weirdness,
+        ),
+    }
 }
 
 pub const fn climate_target(
@@ -517,6 +761,24 @@ pub fn biome_source_from_stem_id(id: &str) -> Option<BiomeSourceModel> {
     }
 }
 
+pub fn multi_noise_parameter_list_preset(
+    id: &str,
+) -> Option<MultiNoiseBiomeSourceParameterListPreset> {
+    match id.strip_prefix("minecraft:").unwrap_or(id) {
+        "nether" => Some(MultiNoiseBiomeSourceParameterListPreset {
+            id: "minecraft:nether",
+            parameters: NETHER_BIOME_PARAMETERS,
+            used_biomes: NETHER_MULTI_NOISE_USED_BIOMES,
+        }),
+        "overworld" => Some(MultiNoiseBiomeSourceParameterListPreset {
+            id: "minecraft:overworld",
+            parameters: OVERWORLD_BIOME_PARAMETERS,
+            used_biomes: OVERWORLD_MULTI_NOISE_USED_BIOMES,
+        }),
+        _ => None,
+    }
+}
+
 pub fn checkerboard_biome_source(
     biomes: Vec<&'static str>,
     scale: i32,
@@ -551,7 +813,7 @@ pub fn select_biome_from_source(
         } => select_climate_biome(NETHER_BIOME_PARAMETERS, climate),
         BiomeSourceModel::MultiNoisePreset {
             preset: "minecraft:overworld",
-        } => None,
+        } => select_climate_biome(OVERWORLD_BIOME_PARAMETERS, climate),
         BiomeSourceModel::MultiNoisePreset { .. } => None,
         BiomeSourceModel::TheEnd => Some(select_end_biome(quart_x, quart_y, quart_z, end_erosion)),
     }
@@ -602,10 +864,11 @@ const fn square(value: i64) -> i64 {
 mod tests {
     use super::{
         biome_source_codec, biome_source_from_stem_id, builtin_biome, checkerboard_biome_source,
-        climate_node_distance, climate_point, climate_target, quantize_coord,
+        climate_node_distance, climate_point, climate_sampler_sample, climate_target,
+        multi_noise_parameter_list_preset, quantize_coord, quart_to_block,
         select_biome_from_source, select_climate_biome, select_end_biome, unquantize_coord,
         BiomeSourceModel, ClimateBiomeEntry, ClimateParameter, ClimateParameterList,
-        BUILTIN_BIOMES, NETHER_BIOME_PARAMETERS,
+        ClimateSamplerInput, BUILTIN_BIOMES, NETHER_BIOME_PARAMETERS, OVERWORLD_BIOME_PARAMETERS,
     };
 
     #[test]
@@ -633,6 +896,35 @@ mod tests {
     }
 
     #[test]
+    fn climate_sampler_sample_uses_quart_to_block_context_before_quantization() {
+        assert_eq!(quart_to_block(7), 28);
+        assert_eq!(quart_to_block(-3), -12);
+
+        let sample = climate_sampler_sample(
+            7,
+            -3,
+            11,
+            ClimateSamplerInput {
+                temperature: 0.25,
+                humidity: -0.125,
+                continentalness: -0.5,
+                erosion: 0.75,
+                depth: 0.0,
+                weirdness: 0.375,
+            },
+        );
+        assert_eq!(sample.block_x, 28);
+        assert_eq!(sample.block_y, -12);
+        assert_eq!(sample.block_z, 44);
+        assert_eq!(sample.target.temperature, 2500);
+        assert_eq!(sample.target.humidity, -1250);
+        assert_eq!(sample.target.continentalness, -5000);
+        assert_eq!(sample.target.erosion, 7500);
+        assert_eq!(sample.target.depth, 0);
+        assert_eq!(sample.target.weirdness, 3750);
+    }
+
+    #[test]
     fn nether_multi_noise_parameters_select_nearest_biome() {
         assert_eq!(NETHER_BIOME_PARAMETERS.len(), 5);
         assert_eq!(
@@ -655,6 +947,72 @@ mod tests {
                 climate_target(-0.5, 0.0, 0.0, 0.0, 0.0, 0.0)
             ),
             Some("minecraft:basalt_deltas")
+        );
+    }
+
+    #[test]
+    fn multi_noise_parameter_list_presets_match_vanilla_known_presets() {
+        let nether = multi_noise_parameter_list_preset("nether").unwrap();
+        assert_eq!(nether.id, "minecraft:nether");
+        assert_eq!(nether.parameters, NETHER_BIOME_PARAMETERS);
+        assert_eq!(
+            nether.used_biomes,
+            &[
+                "minecraft:nether_wastes",
+                "minecraft:soul_sand_valley",
+                "minecraft:crimson_forest",
+                "minecraft:warped_forest",
+                "minecraft:basalt_deltas"
+            ]
+        );
+
+        let overworld = multi_noise_parameter_list_preset("minecraft:overworld").unwrap();
+        assert_eq!(overworld.id, "minecraft:overworld");
+        assert_eq!(overworld.parameters, OVERWORLD_BIOME_PARAMETERS);
+        assert!(overworld.used_biomes.contains(&"minecraft:mushroom_fields"));
+        assert!(overworld.used_biomes.contains(&"minecraft:plains"));
+        assert!(overworld.used_biomes.contains(&"minecraft:deep_dark"));
+        assert!(multi_noise_parameter_list_preset("missing").is_none());
+    }
+
+    #[test]
+    fn overworld_multi_noise_representative_parameters_select_nearest_biome() {
+        assert_eq!(OVERWORLD_BIOME_PARAMETERS.len(), 14);
+        assert_eq!(
+            select_climate_biome(
+                OVERWORLD_BIOME_PARAMETERS,
+                climate_target(0.0, 0.0, -1.1, 0.0, 0.0, 0.0)
+            ),
+            Some("minecraft:mushroom_fields")
+        );
+        assert_eq!(
+            select_climate_biome(
+                OVERWORLD_BIOME_PARAMETERS,
+                climate_target(0.62, -0.2, 0.2, 0.0, 0.0, 0.0)
+            ),
+            Some("minecraft:desert")
+        );
+        assert_eq!(
+            select_climate_biome(
+                OVERWORLD_BIOME_PARAMETERS,
+                climate_target(0.0, 0.0, 0.1, 0.0, 0.0, 0.0)
+            ),
+            Some("minecraft:forest")
+        );
+
+        let overworld = BiomeSourceModel::MultiNoisePreset {
+            preset: "minecraft:overworld",
+        };
+        assert_eq!(
+            select_biome_from_source(
+                &overworld,
+                0,
+                0,
+                0,
+                climate_target(-0.3, -0.5, 0.0, 0.0, 0.0, 0.0),
+                0.0
+            ),
+            Some("minecraft:plains")
         );
     }
 
