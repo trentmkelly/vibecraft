c54ffd37abc4c159e9480da32d3309a921d11962
trentmkelly <trent.michael.kelly@gmail.com>
2026-05-19 06:00:55 -0400

    Expand non-living entity metadata indexes

--- src/entity_metadata.rs
+++ src/entity_metadata.rs
@@ -140,6 +140,166 @@ pub const ZOMBIE_METADATA: &[EntityMetadataField] = &[
     field("Zombie", 18, "DATA_DROWNED_CONVERSION_ID", "BOOLEAN"),
 ];
 
+pub const AREA_EFFECT_CLOUD_METADATA: &[EntityMetadataField] = &[
+    field("AreaEffectCloud", 8, "DATA_RADIUS", "FLOAT"),
+    field("AreaEffectCloud", 9, "DATA_WAITING", "BOOLEAN"),
+    field("AreaEffectCloud", 10, "DATA_PARTICLE", "PARTICLE"),
+];
+
+pub const DISPLAY_METADATA: &[EntityMetadataField] = &[
+    field(
+        "Display",
+        8,
+        "DATA_TRANSFORMATION_INTERPOLATION_START_DELTA_TICKS_ID",
+        "INT",
+    ),
+    field(
+        "Display",
+        9,
+        "DATA_TRANSFORMATION_INTERPOLATION_DURATION_ID",
+        "INT",
+    ),
+    field(
+        "Display",
+        10,
+        "DATA_POS_ROT_INTERPOLATION_DURATION_ID",
+        "INT",
+    ),
+    field("Display", 11, "DATA_TRANSLATION_ID", "VECTOR3"),
+    field("Display", 12, "DATA_SCALE_ID", "VECTOR3"),
+    field("Display", 13, "DATA_LEFT_ROTATION_ID", "QUATERNION"),
+    field("Display", 14, "DATA_RIGHT_ROTATION_ID", "QUATERNION"),
+    field(
+        "Display",
+        15,
+        "DATA_BILLBOARD_RENDER_CONSTRAINTS_ID",
+        "BYTE",
+    ),
+    field("Display", 16, "DATA_BRIGHTNESS_OVERRIDE_ID", "INT"),
+    field("Display", 17, "DATA_VIEW_RANGE_ID", "FLOAT"),
+    field("Display", 18, "DATA_SHADOW_RADIUS_ID", "FLOAT"),
+    field("Display", 19, "DATA_SHADOW_STRENGTH_ID", "FLOAT"),
+    field("Display", 20, "DATA_WIDTH_ID", "FLOAT"),
+    field("Display", 21, "DATA_HEIGHT_ID", "FLOAT"),
+    field("Display", 22, "DATA_GLOW_COLOR_OVERRIDE_ID", "INT"),
+];
+
+pub const BLOCK_DISPLAY_METADATA: &[EntityMetadataField] = &[field(
+    "BlockDisplay",
+    23,
+    "DATA_BLOCK_STATE_ID",
+    "BLOCK_STATE",
+)];
+
+pub const ITEM_DISPLAY_METADATA: &[EntityMetadataField] = &[
+    field("ItemDisplay", 23, "DATA_ITEM_STACK_ID", "ITEM_STACK"),
+    field("ItemDisplay", 24, "DATA_ITEM_DISPLAY_ID", "BYTE"),
+];
+
+pub const TEXT_DISPLAY_METADATA: &[EntityMetadataField] = &[
+    field("TextDisplay", 23, "DATA_TEXT_ID", "COMPONENT"),
+    field("TextDisplay", 24, "DATA_LINE_WIDTH_ID", "INT"),
+    field("TextDisplay", 25, "DATA_BACKGROUND_COLOR_ID", "INT"),
+    field("TextDisplay", 26, "DATA_TEXT_OPACITY_ID", "BYTE"),
+    field("TextDisplay", 27, "DATA_STYLE_FLAGS_ID", "BYTE"),
+];
+
+pub const INTERACTION_METADATA: &[EntityMetadataField] = &[
+    field("Interaction", 8, "DATA_WIDTH_ID", "FLOAT"),
+    field("Interaction", 9, "DATA_HEIGHT_ID", "FLOAT"),
+    field("Interaction", 10, "DATA_RESPONSE_ID", "BOOLEAN"),
+];
+
+pub const HANGING_ENTITY_METADATA: &[EntityMetadataField] =
+    &[field("HangingEntity", 8, "DATA_DIRECTION", "DIRECTION")];
+
+pub const ITEM_FRAME_METADATA: &[EntityMetadataField] = &[
+    field("ItemFrame", 9, "DATA_ITEM", "ITEM_STACK"),
+    field("ItemFrame", 10, "DATA_ROTATION", "INT"),
+];
+
+pub const PAINTING_METADATA: &[EntityMetadataField] = &[field(
+    "Painting",
+    9,
+    "DATA_PAINTING_VARIANT_ID",
+    "PAINTING_VARIANT",
+)];
+
+pub const ARMOR_STAND_METADATA: &[EntityMetadataField] = &[
+    field("ArmorStand", 15, "DATA_CLIENT_FLAGS", "BYTE"),
+    field("ArmorStand", 16, "DATA_HEAD_POSE", "ROTATIONS"),
+    field("ArmorStand", 17, "DATA_BODY_POSE", "ROTATIONS"),
+    field("ArmorStand", 18, "DATA_LEFT_ARM_POSE", "ROTATIONS"),
+    field("ArmorStand", 19, "DATA_RIGHT_ARM_POSE", "ROTATIONS"),
+    field("ArmorStand", 20, "DATA_LEFT_LEG_POSE", "ROTATIONS"),
+    field("ArmorStand", 21, "DATA_RIGHT_LEG_POSE", "ROTATIONS"),
+];
+
+pub const ITEM_ENTITY_METADATA: &[EntityMetadataField] =
+    &[field("ItemEntity", 8, "DATA_ITEM", "ITEM_STACK")];
+
+pub const PRIMED_TNT_METADATA: &[EntityMetadataField] = &[
+    field("PrimedTnt", 8, "DATA_FUSE_ID", "INT"),
+    field("PrimedTnt", 9, "DATA_BLOCK_STATE_ID", "BLOCK_STATE"),
+];
+
+pub const FALLING_BLOCK_ENTITY_METADATA: &[EntityMetadataField] = &[field(
+    "FallingBlockEntity",
+    8,
+    "DATA_START_POS",
+    "BLOCK_POS",
+)];
+
+pub const EYE_OF_ENDER_METADATA: &[EntityMetadataField] =
+    &[field("EyeOfEnder", 8, "DATA_ITEM_STACK", "ITEM_STACK")];
+
+pub const ABSTRACT_ARROW_METADATA: &[EntityMetadataField] = &[
+    field("AbstractArrow", 8, "ID_FLAGS", "BYTE"),
+    field("AbstractArrow", 9, "PIERCE_LEVEL", "BYTE"),
+    field("AbstractArrow", 10, "IN_GROUND", "BOOLEAN"),
+];
+
+pub const ARROW_METADATA: &[EntityMetadataField] = &[field("Arrow", 11, "ID_EFFECT_COLOR", "INT")];
+
+pub const THROWN_TRIDENT_METADATA: &[EntityMetadataField] = &[
+    field("ThrownTrident", 11, "ID_LOYALTY", "BYTE"),
+    field("ThrownTrident", 12, "ID_FOIL", "BOOLEAN"),
+];
+
+pub const FIREWORK_ROCKET_ENTITY_METADATA: &[EntityMetadataField] = &[
+    field(
+        "FireworkRocketEntity",
+        8,
+        "DATA_ID_FIREWORKS_ITEM",
+        "ITEM_STACK",
+    ),
+    field(
+        "FireworkRocketEntity",
+        9,
+        "DATA_ATTACHED_TO_TARGET",
+        "OPTIONAL_UNSIGNED_INT",
+    ),
+    field("FireworkRocketEntity", 10, "DATA_SHOT_AT_ANGLE", "BOOLEAN"),
+];
+
+pub const FISHING_HOOK_METADATA: &[EntityMetadataField] = &[
+    field("FishingHook", 8, "DATA_HOOKED_ENTITY", "INT"),
+    field("FishingHook", 9, "DATA_BITING", "BOOLEAN"),
+];
+
+pub const THROWABLE_ITEM_PROJECTILE_METADATA: &[EntityMetadataField] = &[field(
+    "ThrowableItemProjectile",
+    8,
+    "DATA_ITEM_STACK",
+    "ITEM_STACK",
+)];
+
+pub const FIREBALL_METADATA: &[EntityMetadataField] =
+    &[field("Fireball", 8, "DATA_ITEM_STACK", "ITEM_STACK")];
+
+pub const WITHER_SKULL_METADATA: &[EntityMetadataField] =
+    &[field("WitherSkull", 8, "DATA_DANGEROUS", "BOOLEAN")];
+
 pub const ENTITY_METADATA_CLASSES: &[EntityMetadataClass] = &[
     class("Entity", None, ENTITY_METADATA),
     class("LivingEntity", Some("Entity"), LIVING_ENTITY_METADATA),
@@ -163,6 +323,68 @@ pub const ENTITY_METADATA_CLASSES: &[EntityMetadataClass] = &[
     class("Sniffer", Some("Animal"), SNIFFER_METADATA),
     class("Wolf", Some("TamableAnimal"), WOLF_METADATA),
     class("Zombie", Some("Monster"), ZOMBIE_METADATA),
+    class(
+        "AreaEffectCloud",
+        Some("Entity"),
+        AREA_EFFECT_CLOUD_METADATA,
+    ),
+    class("Display", Some("Entity"), DISPLAY_METADATA),
+    class("BlockDisplay", Some("Display"), BLOCK_DISPLAY_METADATA),
+    class("ItemDisplay", Some("Display"), ITEM_DISPLAY_METADATA),
+    class("TextDisplay", Some("Display"), TEXT_DISPLAY_METADATA),
+    class("Interaction", Some("Entity"), INTERACTION_METADATA),
+    class("BlockAttachedEntity", Some("Entity"), &[]),
+    class(
+        "HangingEntity",
+        Some("BlockAttachedEntity"),
+        HANGING_ENTITY_METADATA,
+    ),
+    class("ItemFrame", Some("HangingEntity"), ITEM_FRAME_METADATA),
+    class("GlowItemFrame", Some("ItemFrame"), &[]),
+    class("Painting", Some("HangingEntity"), PAINTING_METADATA),
+    class("ArmorStand", Some("LivingEntity"), ARMOR_STAND_METADATA),
+    class("ItemEntity", Some("Entity"), ITEM_ENTITY_METADATA),
+    class("PrimedTnt", Some("Entity"), PRIMED_TNT_METADATA),
+    class(
+        "FallingBlockEntity",
+        Some("Entity"),
+        FALLING_BLOCK_ENTITY_METADATA,
+    ),
+    class("EyeOfEnder", Some("Entity"), EYE_OF_ENDER_METADATA),
+    class("Projectile", Some("Entity"), &[]),
+    class("AbstractArrow", Some("Projectile"), ABSTRACT_ARROW_METADATA),
+    class("Arrow", Some("AbstractArrow"), ARROW_METADATA),
+    class("SpectralArrow", Some("AbstractArrow"), &[]),
+    class(
+        "ThrownTrident",
+        Some("AbstractArrow"),
+        THROWN_TRIDENT_METADATA,
+    ),
+    class(
+        "FireworkRocketEntity",
+        Some("Projectile"),
+        FIREWORK_ROCKET_ENTITY_METADATA,
+    ),
+    class("FishingHook", Some("Projectile"), FISHING_HOOK_METADATA),
+    class("ShulkerBullet", Some("Projectile"), &[]),
+    class("LlamaSpit", Some("Projectile"), &[]),
+    class("ThrowableProjectile", Some("Projectile"), &[]),
+    class(
+        "ThrowableItemProjectile",
+        Some("ThrowableProjectile"),
+        THROWABLE_ITEM_PROJECTILE_METADATA,
+    ),
+    class(
+        "Fireball",
+        Some("AbstractHurtingProjectile"),
+        FIREBALL_METADATA,
+    ),
+    class("AbstractHurtingProjectile", Some("Projectile"), &[]),
+    class(
+        "WitherSkull",
+        Some("AbstractHurtingProjectile"),
+        WITHER_SKULL_METADATA,
+    ),
 ];
 
 pub fn metadata_class(class_name: &str) -> Option<&'static EntityMetadataClass> {
@@ -296,6 +518,101 @@ mod tests {
         assert_eq!(metadata_class("PathfinderMob").unwrap().fields, &[]);
         assert_eq!(metadata_class("Monster").unwrap().fields, &[]);
         assert_eq!(metadata_class("Animal").unwrap().fields, &[]);
+        assert_eq!(metadata_class("Projectile").unwrap().fields, &[]);
+        assert_eq!(metadata_class("BlockAttachedEntity").unwrap().fields, &[]);
         assert!(metadata_class("Missing").is_none());
+    }
+
+    #[test]
+    fn non_living_entity_metadata_starts_after_root_entity_fields() {
+        let item = inherited_metadata_fields("ItemEntity").unwrap();
+        assert_eq!(item[8].accessor, "DATA_ITEM");
+        assert_eq!(item[8].serializer, "ITEM_STACK");
+
+        let tnt = inherited_metadata_fields("PrimedTnt").unwrap();
+        assert_eq!(tnt[8].accessor, "DATA_FUSE_ID");
+        assert_eq!(tnt[9].serializer, "BLOCK_STATE");
+
+        let falling_block = inherited_metadata_fields("FallingBlockEntity").unwrap();
+        assert_eq!(falling_block[8].accessor, "DATA_START_POS");
+        assert_eq!(falling_block[8].serializer, "BLOCK_POS");
+
+        let area_effect_cloud = inherited_metadata_fields("AreaEffectCloud").unwrap();
+        assert_eq!(area_effect_cloud[8].accessor, "DATA_RADIUS");
+        assert_eq!(area_effect_cloud[10].serializer, "PARTICLE");
+
+        let interaction = inherited_metadata_fields("Interaction").unwrap();
+        assert_eq!(interaction[8].accessor, "DATA_WIDTH_ID");
+        assert_eq!(interaction[10].accessor, "DATA_RESPONSE_ID");
+    }
+
+    #[test]
+    fn display_entity_subclasses_continue_after_display_base_indexes() {
+        let display = inherited_metadata_fields("Display").unwrap();
+        assert_eq!(
+            display[8].accessor,
+            "DATA_TRANSFORMATION_INTERPOLATION_START_DELTA_TICKS_ID"
+        );
+        assert_eq!(display[11].serializer, "VECTOR3");
+        assert_eq!(display[14].serializer, "QUATERNION");
+        assert_eq!(display[22].accessor, "DATA_GLOW_COLOR_OVERRIDE_ID");
+
+        let block_display = inherited_metadata_fields("BlockDisplay").unwrap();
+        assert_eq!(block_display[23].accessor, "DATA_BLOCK_STATE_ID");
+        assert_eq!(block_display[23].serializer, "BLOCK_STATE");
+
+        let item_display = inherited_metadata_fields("ItemDisplay").unwrap();
+        assert_eq!(item_display[23].accessor, "DATA_ITEM_STACK_ID");
+        assert_eq!(item_display[24].accessor, "DATA_ITEM_DISPLAY_ID");
+
+        let text_display = inherited_metadata_fields("TextDisplay").unwrap();
+        assert_eq!(text_display[23].accessor, "DATA_TEXT_ID");
+        assert_eq!(text_display[27].accessor, "DATA_STYLE_FLAGS_ID");
+    }
+
+    #[test]
+    fn hanging_and_decorative_entities_include_parent_accessors() {
+        let item_frame = inherited_metadata_fields("ItemFrame").unwrap();
+        assert_eq!(item_frame[8].accessor, "DATA_DIRECTION");
+        assert_eq!(item_frame[9].accessor, "DATA_ITEM");
+        assert_eq!(item_frame[10].accessor, "DATA_ROTATION");
+
+        let glow_item_frame = inherited_metadata_fields("GlowItemFrame").unwrap();
+        assert_eq!(glow_item_frame, item_frame);
+
+        let painting = inherited_metadata_fields("Painting").unwrap();
+        assert_eq!(painting[8].serializer, "DIRECTION");
+        assert_eq!(painting[9].serializer, "PAINTING_VARIANT");
+
+        let armor_stand = inherited_metadata_fields("ArmorStand").unwrap();
+        assert_eq!(armor_stand[14].accessor, "SLEEPING_POS_ID");
+        assert_eq!(armor_stand[15].accessor, "DATA_CLIENT_FLAGS");
+        assert_eq!(armor_stand[21].accessor, "DATA_RIGHT_LEG_POSE");
+    }
+
+    #[test]
+    fn projectile_metadata_matches_java_subclass_offsets() {
+        let arrow = inherited_metadata_fields("Arrow").unwrap();
+        assert_eq!(arrow[8].accessor, "ID_FLAGS");
+        assert_eq!(arrow[10].accessor, "IN_GROUND");
+        assert_eq!(arrow[11].accessor, "ID_EFFECT_COLOR");
+
+        let trident = inherited_metadata_fields("ThrownTrident").unwrap();
+        assert_eq!(trident[11].accessor, "ID_LOYALTY");
+        assert_eq!(trident[12].accessor, "ID_FOIL");
+
+        let firework = inherited_metadata_fields("FireworkRocketEntity").unwrap();
+        assert_eq!(firework[8].serializer, "ITEM_STACK");
+        assert_eq!(firework[9].serializer, "OPTIONAL_UNSIGNED_INT");
+        assert_eq!(firework[10].serializer, "BOOLEAN");
+
+        let thrown_item = inherited_metadata_fields("ThrowableItemProjectile").unwrap();
+        assert_eq!(thrown_item[8].accessor, "DATA_ITEM_STACK");
+
+        let fireball = inherited_metadata_fields("Fireball").unwrap();
+        assert_eq!(fireball[8].serializer, "ITEM_STACK");
+
+        let wither_skull = inherited_metadata_fields("WitherSkull").unwrap();
+        assert_eq!(wither_skull[8].accessor, "DATA_DANGEROUS");
     }
 }
