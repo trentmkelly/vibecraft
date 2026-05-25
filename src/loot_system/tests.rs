8ef6ba231ca4df61db6f3335fb547143ace706c0
trentmkelly <trent.michael.kelly@gmail.com>
2026-05-25 19:32:17 -0400

    Split loot predicate coverage setup

--- src/loot_system/tests.rs
+++ src/loot_system/tests.rs
@@ -1,7 +1,5 @@
 use super::*;
-use crate::advancement_system::{
-    AdvancementDefinition, AdvancementRewards, PlayerAdvancementSet,
-};
+use crate::advancement_system::{AdvancementDefinition, AdvancementRewards, PlayerAdvancementSet};
 use crate::registry::Identifier;
 
 fn table_with_pool(pool: LootPool) -> LootTable {
@@ -333,6 +331,13 @@ fn number_providers_cover_java_26_1_2_provider_registry() {
 
 #[test]
 fn loot_predicates_cover_java_condition_surface() {
+    let context = java_condition_surface_context();
+    let condition = java_condition_surface_condition();
+
+    assert!(condition.matches(&context));
+}
+
+fn java_condition_surface_context() -> LootContext {
     let mut context = LootContext::new(LootParamSet::AllParams, 13);
     context.insert_param(LootParamValue::BlockState("minecraft:oak_log".to_string()));
     context.insert_param(LootParamValue::Tool("minecraft:diamond_axe".to_string()));
@@ -361,8 +366,11 @@ fn loot_predicates_cover_java_condition_surface() {
     context
         .condition_references
         .insert("minecraft:ok".to_string());
+    context
+}
 
-    let condition = LootCondition::AllOf(vec![
+fn java_condition_surface_condition() -> LootCondition {
+    LootCondition::AllOf(vec![
         LootCondition::AnyOf(vec![
             LootCondition::RandomChance(1.0),
             LootCondition::RandomChanceWithEnchantedBonus {
@@ -433,9 +441,7 @@ fn loot_predicates_cover_java_condition_surface() {
         LootCondition::TableBonus {
             chances: vec![0.0, 0.0, 1.0],
         },
-    ]);
-
-    assert!(condition.matches(&context));
+    ])
 }
 
 #[test]
@@ -668,8 +674,7 @@ fn random_sequences_are_repeatable_and_independent_from_context_seed_stream() {
     let mut first = LootContext::new(LootParamSet::Chest, 99);
     let mut second = LootContext::new(LootParamSet::Chest, 99);
     let mut different_sequence = table.clone();
-    different_sequence.random_sequence =
-        Some("minecraft:chests/abandoned_mineshaft".to_string());
+    different_sequence.random_sequence = Some("minecraft:chests/abandoned_mineshaft".to_string());
 
     assert_eq!(table.evaluate(&mut first), table.evaluate(&mut second));
     assert_ne!(
@@ -786,8 +791,7 @@ fn behavior_engine_covers_entity_fishing_archaeology_reward_gift_barter_and_comm
     entity.killed_by_player = true;
     assert_eq!(engine.resolve(entity, 1).param_set, LootParamSet::Entity);
 
-    let mut fishing =
-        LootRequest::new(LootSurface::FishingRetrieve, "minecraft:gameplay/fishing");
+    let mut fishing = LootRequest::new(LootSurface::FishingRetrieve, "minecraft:gameplay/fishing");
     fishing.tool = Some("minecraft:fishing_rod".to_string());
     assert_eq!(engine.resolve(fishing, 1).param_set, LootParamSet::Fishing);
 
@@ -949,6 +953,5 @@ fn advancement_reward_loot_grants_xp_and_tables_with_player_context() {
         )
     );
 }
-
 
 mod advanced_tests;
